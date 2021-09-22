be14912d4ba27b530db5dc261667d3010f6b47f1
Willyan Guimaraes Caetano <willyan.caetano@picpay.com>
2021-09-22 19:41:23 -0300

    feat: initial commit

---
+++ src/test/java/com/github/willyancaetano/planets/api/PlanetsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.willyancaetano.planets.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanetsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
